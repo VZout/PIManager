ff18dab269f9540c386f54524254b33743297af3
unknown <bideblade1@hotmail.com>
2015-09-28 18:21:50 +0200

    testing user input

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<html>
+<body>
+<form action="functioncalling.php">
+    <input type="text" name="txt" />
+    <input type="submit" name="insert" value="insert" onclick="insert()" />
+    <input type="submit" name="select" value="select" onclick="select()" />
+</form>
+
 <?php
-     echo "PHP Server Test";
+function select(){
+   echo "The select function is called.";
+}
+function insert(){
+   echo "The insert function is called.";
+}
 ?>
